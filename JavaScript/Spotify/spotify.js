5f0c15ed85258efc98a9016f7428746d276c29b2
AlCombes <alexis.combes@gmail.com>
2024-05-16 16:12:16 +0200

    Updated TP spotify

--- JavaScript/Spotify/spotify.js
+++ JavaScript/Spotify/spotify.js
@@ -2,7 +2,7 @@
 const headers = {
   "Content-Type": "application/json",
   Authorization:
-    "Bearer BQALJjKcHlCrfCuDgbUCIBvn6QzlTQAokDELj1sE_4IX9LU716aEeEOf8VqpBjY-GqTKabctxRnCG-oSQ3j_zkfRGOCMINzLxU1uIsPNItJRO8Kazb4",
+    "Bearer BQAIm1luRCfEEIlH2HAHadfnG-4qAU0Lni4CAGu6yg9frIP4lcMDWDDePHUrLxlcZ54z1by0wZuCVUwxw9zGl5B7PYwkq_EuLcmcuNowt6CXMO1IcnY",
 };
 const fetchOptions = { headers: headers };
 // l'url de recherche d'albums
@@ -16,14 +16,66 @@ function chercherAlbums(event) {
   // empecher le rechargement de la page
   event.preventDefault();
 
-  let motcle = "jardin";
+  let motcle = document.getElementById("motcle").value;
+
+  let resultElt = document.getElementById("results");
+  resultElt.innerHTML = "";
 
   fetch(url + motcle, fetchOptions)
     .then((response) => {
       return response.json();
     })
     .then((dataJSON) => {
+      // Traiter ici le résultat de la requette
       console.log(dataJSON);
+      dataJSON.albums.items.forEach((element) => {
+        console.log(element.name);
+        let albumId = element.id;
+        let imageSrc = element.images[2].url;
+        console.log(imageSrc);
+        resultElt.innerHTML +=
+          "<img id='" + element.id + "' src='" + imageSrc + "'/>";
+      });
+
+      let imgList = document.getElementsByTagName("img");
+      console.log(imgList);
+      for (let imageBox of imgList) {
+        imageBox.addEventListener("click", function (event) {
+          console.log(imageBox.id);
+          provideAlbumsDetails(imageBox.id);
+        });
+      }
+    })
+    .catch((error) => console.log(error));
+}
+
+const urlAlbumDetails = "https://api.spotify.com/v1/albums/";
+
+function provideAlbumsDetails(albumId) {
+  fetch(urlAlbumDetails + albumId, fetchOptions)
+    .then((response) => {
+      return response.json();
+    })
+    .then((dataJSON) => {
+      // Traiter ici le résultat de la requette
+      console.log(dataJSON);
+      document.getElementById("disque").innerText = dataJSON.name;
+      document.getElementById("chanteur").innerText = dataJSON.artists[0].name;
+      // document.getElementById("disque").innerText = dataJSON.name;
+
+      let res = "<ul>";
+      for (let song of dataJSON.tracks.items) {
+        res += '<li id="' + song.id + '">' + song.name;
+        if (song.preview_url != null) {
+          res +=
+            "<audio id='AudioSong' controls src='" +
+            song.preview_url +
+            "'></audio>";
+        }
+        res += "</li>";
+      }
+      document.getElementById("chansons").innerHTML = res;
+      console.log(res);
     })
     .catch((error) => console.log(error));
 }
