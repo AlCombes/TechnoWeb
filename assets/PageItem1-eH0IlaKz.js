a16372a37676014d62a643c209ece766f6555699
AlCombes <alexis.combes@gmail.com>
2024-05-30 09:22:31 +0200

    Updates

--- assets/PageItem1-eH0IlaKz.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as t,e as a,w as c,V as o,b as r}from"./index-ByWwUkz2.js";const n={};function s(_,f){return t(),a(o,null,{default:c(()=>[r("Item 1")]),_:1})}const d=e(n,[["render",s]]);export{d as default};
--- assets/index-BaEikMiL.js
+++
@@ -1,1 +0,0 @@
-import{o as e,c as n,a as s,w as r,b as _,V as l,F as m,d as p,e as u}from"./index-ByWwUkz2.js";const i="/TechnoWeb/cinematheque.jpg",d=p("img",{src:i},null,-1),h={__name:"HelloWorld",setup(t){return(o,a)=>(e(),n(m,null,[s(l,null,{default:r(()=>[_(" The movies AC v2 ")]),_:1}),d],64))}},f={__name:"index",setup(t){return(o,a)=>{const c=h;return e(),u(c)}}};export{f as default};
