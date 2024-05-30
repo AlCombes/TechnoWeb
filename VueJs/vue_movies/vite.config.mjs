e25920c4ef6761ea390a34bdb7c124bf93470460
AlCombes <alexis.combes@gmail.com>
2024-05-30 08:50:34 +0200

    to save

--- VueJs/vue_movies/vite.config.mjs
+++ VueJs/vue_movies/vite.config.mjs
@@ -11,7 +11,7 @@ import { fileURLToPath, URL } from "node:url";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/TechnoWeb/VueJs/vue_movies/",
+  base: "/TechnoWeb/",
   plugins: [
     VueRouter(),
     Vue({
